1c32ea34c269ed7eaed7703ee1b00292357d8188
chau1711996 <chau1711996@gmail.com>
2021-07-12 14:04:23 +0700

    feat(list): load list for recycleview has native ads small

--- app/src/main/java/dev/chau/adsmodjava/fragment/VerticalFragment.java
+++ app/src/main/java/dev/chau/adsmodjava/fragment/VerticalFragment.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 
 import dev.chau.adsmodjava.AdsManager;
 import dev.chau.adsmodjava.R;
+import dev.chau.adsmodjava.adapter.ItemAdapter;
 
 public class VerticalFragment extends Fragment {
     private static final String TAG = "LogVerticalFragment";
@@ -22,6 +23,7 @@ public class VerticalFragment extends Fragment {
     TemplateView templateView;
     RecyclerView rcvVer;
     ArrayList<String> mList = new ArrayList<>();
+    ItemAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +35,7 @@ public class VerticalFragment extends Fragment {
         rcvVer = view.findViewById(R.id.rcvVer);
 
         setUpAdsNative();
+        setUpRecycleViewVer();
 
         return view;
     }
@@ -40,6 +43,38 @@ public class VerticalFragment extends Fragment {
     private void setUpAdsNative() {
         AdsManager adsManager = AdsManager.getInstance(getContext(), TAG);
         adsManager.loadNative("ca-app-pub-3940256099942544/2247696110", templateView);
+    }
+
+    private void setUpRecycleViewVer(){
+        mList = loadList();
+
+        adapter = new ItemAdapter(mList, getContext(), randomNum());
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        rcvVer.setLayoutManager(manager);
+        rcvVer.setAdapter(adapter);
+    }
+
+    private int randomNum(){
+        int min = 3;
+        int max = 10;
+
+        int result = -1;
+
+        while (result % 2 != 0){
+            result = (int)(Math.random() * (max - min + 1) + min);
+        }
+
+        return result;
+    }
+
+    private ArrayList<String> loadList(){
+        ArrayList<String> list = new ArrayList<>();
+
+        for(int i = 1; i <= 20; i++){
+            list.add("Chau "+i);
+        }
+
+        return list;
     }
 
 }
